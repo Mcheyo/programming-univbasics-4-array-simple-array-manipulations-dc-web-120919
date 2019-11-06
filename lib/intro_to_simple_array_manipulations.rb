c4d9225b509f37f60ccff5ba42af4f11532ce561
Mansour Cheyo <mcheyo.mc@gmail.com>
2019-11-06 17:48:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -45,7 +45,7 @@ def using_insert(list_of_programming_languages, new_array)
   list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
   another_language = "Python"
   new_array = list_of_programming_languages.insert(another_language)
-end 
+end
 
-  
+
   
