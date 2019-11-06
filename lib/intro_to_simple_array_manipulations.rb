92b465af5a669a5815652a56c4ffd513c3a215fc
Mansour Cheyo <mcheyo.mc@gmail.com>
2019-11-06 18:01:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -60,6 +60,6 @@ end
 def using_delete(instructors)
     instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
     no_offense_steven = instructors.delete
-  end 
-  
+  end
+
     
