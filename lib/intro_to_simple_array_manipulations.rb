8c0382e440c4ca98b1b4ab98f569e2510e469e52
Mansour Cheyo <mcheyo.mc@gmail.com>
2019-11-06 17:48:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -40,3 +40,12 @@ end
     more_favs = ["sports cars", "flatiron school"]
   all_my_favs =  my_favorite_things.concat(more_favs)
   end
+
+def using_insert(list_of_programming_languages, new_array)
+  list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+  another_language = "Python"
+  new_array = list_of_programming_languages.insert(another_language)
+end 
+
+  
+  
